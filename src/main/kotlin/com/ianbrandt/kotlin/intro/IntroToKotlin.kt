43a17a10e152bf0d7bee622872e60e14c60d8c6f
Ian Brandt <ian@ianbrandt.com>
2018-10-10 23:15:10 -0700

    Improved intro.

--- src/main/kotlin/com/ianbrandt/kotlin/intro/IntroToKotlin.kt
+++ src/main/kotlin/com/ianbrandt/kotlin/intro/IntroToKotlin.kt
@@ -9,7 +9,7 @@ package com.ianbrandt.kotlin.intro
 
 
 
-//	Let's learn a bit about about Kotlin...
+// Ready for a lightning-quick intro to Kotlin?! ⚡️
 
 
 
@@ -31,7 +31,8 @@ package com.ianbrandt.kotlin.intro
 
 
 
-// Kotlin has good ole' classes ✅
+// Let's start somewhere familiar for Java developers.
+// Kotlin has classes. ✅
 
 
 
@@ -50,7 +51,7 @@ package com.ianbrandt.kotlin.intro
 
 
 
-// Kotlin has first-class, top-level, functions and variables ✅
+// But then Kotlin has first-class, top-level, functions and variables too. ✅
 
 private const val MESSAGE = "Hello, World!"
 
@@ -75,8 +76,8 @@ fun main() {
 
 
 
-// Kotlin has type and semicolon inference, super strings,
-// and a cool type system ✅
+// Kotlin has type and semicolon inference, super-duper strings,
+// and a pretty nice type system. ✅
 
 
 
@@ -98,7 +99,7 @@ fun main() {
 
 
 
-// ...a type system so cool, it even helps with null safety ✅
+// ...in fact, the type system even helps with null safety. ✅
 
 
 
@@ -121,7 +122,7 @@ fun main() {
 
 
 
-// Functions are expressive in Kotlin ✅
+// Functions are quite expressive in Kotlin. ✅
 
 
 
@@ -145,7 +146,7 @@ fun main() {
 
 
 // Now the real fun begins, Kotlin has data classes
-// and destructuring declarations
+// and destructuring declarations.
 
 
 
@@ -160,27 +161,27 @@ fun main() {
 
 
 
-// Kotlin has nearly seamless Java interoperability
+// Kotlin has nearly seamless Java interoperability.
 
-// Equality just works in Kotlin
+// Equality just works in Kotlin.
 
-// Kotlin has 'when', which is better than switch
+// Kotlin has 'when', which is better than switch.
 
-// In Kotlin, 'if', 'when', 'try' are expressions
+// In Kotlin, 'if', 'when', 'try' are expressions.
 
-// Kotlin has the singleton pattern built in
+// Kotlin has the singleton pattern built in.
 
-// Kotlin has the delegate pattern built in
+// Kotlin has the delegate pattern built in.
 
-// Kotlin banishes static to /dev/null, where it belongs
+// Kotlin banishes static to the ninth circle of /dev/null.
 
-// Kotlin has extension functions, inline functions with
-// reified type parameters, infix functions, function types
+// Kotlin has extension functions, infix functions, inline
+// functions with reified type parameters, function types
 // with receiver, select operator overloading, and all that
-// adds up to type-safe, internal DSLs!
+// adds up to type-safe, internal builders and DSLs.
 
-// Kotlin makes concurrency readable with coroutines!
+// Kotlin makes concurrency readable with coroutines.
 
 // Kotlin goes native, and multi-platform, with
 // Kotlin/Native (iOS, macOS, Windows, Linux, WASM, IoT)
-// and Kotlin/JS (browser, NodeJS)
+// and Kotlin/JS (browser, NodeJS).
