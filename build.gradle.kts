c7614f3d2caa9ee6fa488c71b1ee4d6946835f9c
Ian Brandt <ian@ianbrandt.com>
2018-10-20 20:00:09 -0700

    Removed unused dependencies from the build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 import org.gradle.api.JavaVersion.*
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -20,13 +18,7 @@ repositories {
 }
 
 dependencies {
-	val junitVersion = "5.3.1"
-
 	implementation(kotlin("stdlib-jdk8"))
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.30.2")
-	testImplementation("org.assertj:assertj-core:3.11.1")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 java {
@@ -38,13 +30,6 @@ tasks {
 
 	withType<KotlinCompile> {
 		kotlinOptions.jvmTarget = VERSION_1_8.toString()
-	}
-
-	getByName<Test>("test") {
-		useJUnitPlatform()
-		testLogging {
-			events(PASSED, SKIPPED, FAILED, STANDARD_OUT, STANDARD_ERROR)
-		}
 	}
 
 	getByName<Wrapper>("wrapper") {
